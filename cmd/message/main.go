a9602dc5fdc377f2e0e516ba31ed4f49c9c206a4
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-01-17 23:30:20 +0100

    Gateway now depends on the services protobuf files

--- cmd/message/main.go
+++ cmd/message/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/dietzy1/chatapp/config"
+import (
+	"github.com/dietzy1/chatapp/config"
+	"github.com/dietzy1/chatapp/services/apigateway/server"
+	"github.com/dietzy1/chatapp/services/auth/domain"
+	"github.com/dietzy1/chatapp/services/user/adapters/repository"
+)
 
 func main() {
 
 	config.ReadEnvfile()
 
+	repo := repository.New()
+
+	domain := domain.New(repo)
+
+	server.Start(domain)
 }
