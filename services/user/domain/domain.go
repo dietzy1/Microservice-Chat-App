6b4f6217b73eb70924121aca394cf0effecc0570
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-06-19 08:28:47 +0200

    feat: move of icon domain logic

--- services/user/domain/domain.go
+++ services/user/domain/domain.go
@@ -2,11 +2,9 @@ package domain
 
 type Domain struct {
 	user user
-	cdn  cdn
-	icon icon
 }
 
 // New creates a new application
-func New(user user, icon icon, cdn cdn) *Domain {
-	return &Domain{user: user, icon: icon, cdn: cdn}
+func New(user user) *Domain {
+	return &Domain{user: user}
 }
