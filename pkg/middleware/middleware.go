61c8b56b685d670b5f02c8fbeaeaa905b4564d04
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-06-21 11:36:53 +0200

    feat: new middleware zap logger

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -2,18 +2,15 @@ package middleware
 
 import (
 	"context"
-	"log"
 
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
-func LoggingMiddleware(
-	ctx context.Context,
-	req interface{},
-	info *grpc.UnaryServerInfo,
-	handler grpc.UnaryHandler,
-) (resp interface{}, err error) {
-	log.Printf("gRPC method: %s", info.FullMethod)
-	resp, err = handler(ctx, req)
-	return
+func LoggingMiddleware(logger *zap.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		logger.Info("gRPC method", zap.String("method", info.FullMethod))
+		resp, err := handler(ctx, req)
+		return resp, err
+	}
 }
